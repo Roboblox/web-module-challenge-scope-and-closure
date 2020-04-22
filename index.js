9b8df0833b8c5e8c3ad10941189d892d16cba484
roboblox <supeecyborg@gmail.com>
2020-04-21 21:13:57 -0400

    finished

--- index.js
+++ index.js
@@ -2,24 +2,23 @@
 
 /**
  * ### Challenge `processFirstItem`
- * 
+ *
  * @instructions
  * Implement a higher-order function called `processFirstItem`.
  * It takes two arguments:
  * @param stringList an array of strings.
  * @param callback function that takes a string as its argument.
  * @returns the result of invoking `callback` with the FIRST element in `stringList`.
- * 
+ *
  * Example of usage of this higher-order function:
  * Invoking `processFirstItem` passing `['foo', 'bar']` and `(str) => str + str`,
  * should return 'foofoo'.
-*/
+ */
 function processFirstItem(stringList, callback) {
-  return callback(stringList[0])
+  return callback(stringList[0]);
 }
 
 // ⭐️ Example Challenge END ⭐️
-
 
 ///// M V P ///////
 
@@ -28,9 +27,19 @@ function processFirstItem(stringList, callback) {
  * 
  * 1. What is the difference between counter1 and counter2?
  * 
+ * counter 2 is using a global variable.
+ * counter 1 has a global variable returned as a function.
+ *  and returning a function inside.
+ 
  * 2. Which of the two uses a closure? How can you tell?
- * 
+
+ * counter 1. because it is returning a function from within.
+
  * 3. In what scenario would the counter1 code be preferable? In what scenario would counter2 be better? 
+
+ counter1 is preferable when the function needs to store memory of itself.
+
+ counter2 is preferable when you want to be simple and concise. And not add complexity when it is not needed.
  *
 */
 
@@ -38,8 +47,8 @@ function processFirstItem(stringList, callback) {
 function counterMaker() {
   let count = 0;
   return function counter() {
-   return count++;
-  }
+    count++;
+  };
 }
 
 const counter1 = counterMaker();
@@ -51,36 +60,37 @@ function counter2() {
   return count++;
 }
 
-
 /* Task 2: inning() 
 
 Write a function called `inning` that generates a random number of points that a team scored in an inning. This should be a whole number between 0 and 2. */
 
-function inning(/*Code Here*/){
+function inning() {
+  let score = [];
 
-    /*Code Here*/
+  score = Math.floor(Math.random() * 3);
 
+  return score;
 }
-
+console.log(inning());
 /* Task 3: finalScore()
 
 Write a higher order function called `finalScore` that accepts the callback function `inning` (from above) and a number of innings and and returns the final score of the game in the form of an object.
 
-For example, 
+For example, */
 
-finalScore(inning, 9) might return: 
-{
-  "Home": 11,
-  "Away": 5,
+function finalScore(callback, innings) {
+  // totalScore = { home: 0, away: 0 };
+  let teamScore1 = 0;
+  let teamScore2 = 0;
+
+  for (let i = 0; i < innings; i++) {
+    teamScore1 += inning();
+    teamScore2 += inning();
+  }
+
+  return { teamScore1, teamScore2 };
 }
-
-*/ 
-
-function finalScore(/*code Here*/){
-
-  /*Code Here*/
-
-}
+console.log(finalScore(inning, 9));
 
 /* Task 4: 
 
@@ -90,7 +100,7 @@ Create a function called `scoreboard` that accepts the following parameters:
 (2) Callback function `inning`
 (2) A number of innings
 
-and returns the score at each pont in the game, like so:
+and returns the score at each point in the game, like so:
 
 1st inning: awayTeam - homeTeam
 2nd inning: awayTeam - homeTeam
@@ -103,9 +113,25 @@ and returns the score at each pont in the game, like so:
 9th inning: awayTeam - homeTeam
 
 Final Score: awayTeam - homeTeam */
+function getInningScore(anything) {
+  let home = anything();
 
-function scoreboard(/* CODE HERE */) {
-  /* CODE HERE */
+  let away = anything();
+
+  return { home, away };
 }
-
-
+console.log(getInningScore(inning));
+function scoreBoard(getInningScore, inning, innings) {
+  let home = 0;
+  let away = 0;
+  for (let i = 0; i < innings; i++) {
+    const score = getInningScore(inning);
+    home += score.home;
+    away += score.away;
+    console.log(
+      "inning: " + (i + 1) + " awayTeam:" + away + " homeTeam:" + home
+    );
+  }
+  return { home, away };
+}
+console.log(scoreBoard(getInningScore, inning, 9));
